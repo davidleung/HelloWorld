74966023c4d7adb67911784cc2840feaedf4df44
David Leung <kyleung.david@gmail.com>
2011-04-12 10:43:33 +0800

    Testing New Project
    Signed-off-by: David Leung <kyleung.david@gmail.com>

---
+++ src/com/hippo/appsinfo/AppsInfo.java
@@ -0,0 +1,13 @@
+package com.hippo.appsinfo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AppsInfo extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
